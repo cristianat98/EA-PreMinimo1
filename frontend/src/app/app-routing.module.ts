21648ae4ad60f6dd149e445df7adfd4d691da575
cristianat98 <cristianarmestotejedo@gmail.com>
2020-11-18 12:37:59 +0100

    Clases declaradas en frontend

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PrincipalComponent } from './principal/principal.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/principal' },
+  { path: 'principal', component: PrincipalComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
